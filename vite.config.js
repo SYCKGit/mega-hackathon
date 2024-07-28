65b7fa6f1b606e6ebc59d686cd68d23412ceaabb
SYCKGit <oviyangandhi@gmail.com>
2024-07-29 02:35:58 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        bfs: resolve(__dirname, 'bfs/index.html')
-      }
-    }
-  }
+  plugins: [react()]
 })
