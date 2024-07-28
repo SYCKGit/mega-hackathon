f81cb57b8d55a3761cd0f761d7622cc9be4fb530
Samik Goyal <samikgoyal@gmail.com>
2024-07-28 16:40:49 +0530

    Added DFS page (Samwar's Commit)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <header>
-        <h1>Graph Algorithms Interactive Learning</h1>
-      </header>
-      <main>
-        <section id="bfs">
-          <h2>BFS</h2>
-          <p>Learn about Breadth-First Search (BFS).</p>
-          <a href="bfs">Go to BFS Algorithm Page</a>
-        </section>
-      </main>
-      <footer>
-        <p>&copy; 2024 Graph Algorithms Interactive Learning</p>
-      </footer>
-    </>
-  )
+    return (
+        <>
+            <header>
+                <h1>Graph Algorithms Interactive Learning</h1>
+            </header>
+            <main>
+                <section id="bfs">
+                    <h2>BFS</h2>
+                    <p>Learn about Breadth-First Search (BFS).</p>
+                    <a href="bfs">Go to BFS Algorithm Page</a>
+                </section>
+                <section id="dfs">
+                    <h2>DFS</h2>
+                    <p>Learn about Depth-First Search (DFS).</p>
+                    <a href="dfs">Go to DFS Algorithm Page</a>
+                </section>
+
+            </main>
+            <footer>
+                <p>&copy; 2024 Graph Algorithms Interactive Learning</p>
+            </footer>
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './styles.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Bfs from './routes/Bfs.jsx';
+import Dfs from './routes/Dfs.jsx';
+
 
 const router = createBrowserRouter([
   {
@@ -13,11 +15,16 @@ const router = createBrowserRouter([
   {
     path: '/bfs',
     element: <Bfs />
+  },
+  {
+    path: '/dfs',
+    element: <Dfs />
   }
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
 )
